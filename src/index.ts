e9d54eff27d7749cbea4313fb48b20550b2939b7
Umar Muhammad Aminu <khalifaumar308@gmail.com>
2023-12-17 11:04:53 +0100

    Update index

--- src/index.ts
+++ src/index.ts
@@ -11,39 +11,9 @@ import { Request } from "express";
 // import use
 dotenv.config();
 
-// const allowedOrigins = ["http://127.0.0.1:5173",
-//   "http://localhost:5173",
-//   "https://northfield-frontend.vercel.app/",
-//   "https://northfield-frontend.vercel.app",
-// ];
-
-// const credentials = (req, res, next) => {
-//     const origin = req.headers.origin;
-//     if (allowedOrigins.includes(origin)) {
-//         res.header('Access-Control-Allow-Credentials', true);
-//     }
-//     next();
-// }
-
-
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     optionsSuccessStatus: 200
-// }
-
 const app = express();
 app.use(express.json());
-// app.use(credentials);
-// app.use(credentials);
 app.use(cors());
-// app.use(cors());
-// app.use(cors())
 app.use(function (req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
   res.set('Access-Control-Allow-Origin', 'https://northfield-frontend.vercel.app');
@@ -65,32 +35,5 @@ mongoose
 
 app.use("/user", userRouter);
 
-// const port = 3000;
-// app.listen(port, () => {
-//   console.log(`app running on port ${port}.`);
-// });
-
-
-// app.post("/", async (req, res) => {
-//   try {
-//     for (let index = 0; index < 2; index++) {
-//       const result = await firstTemplate(req.body);
-//       const rt = await sendMail({ name: 'umar', email: 'umaraminudkd@gmail.com', schoolName: 'my school' }, result) 
-//       // console.log(rt, 123456788)
-//     }
-//     return res.status(200).json({msg:"Done"})
-//   } catch (error) {
-//     return res.status(400).json({error:error})
-    
-//   }
-  
-//     // Setting up the response headers
-//     // res.setHeader("Content-Type", "application/pdf");
-//     // res.setHeader("Content-Disposition", `attachment; filename=export.pdf`);
-  
-//     // // Streaming our resulting pdf back to the user
-//   // result.pipe(res);
-// }
-// );
 
 app.post('/sendresult', verifyToken, studentController);
